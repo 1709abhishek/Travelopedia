8f1568467db874cd14b242f497be4ab8a309cdc2
1709abhishek <1709abhishek@gmail.com>
2024-11-19 01:46:52 -0500

    update backend code

---
+++ server/customer-service/src/main/java/com/travelopedia/fun/customer_service/accounts/models/Account.java
@@ -0,0 +1,33 @@
+package com.travelopedia.fun.customer_service.accounts.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+// import jakarta.annotation.Generated;
+
+@Data
+@Entity
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String city;
+    private String country;
+    private String bio;
+    private String favTravelQuote;
+    private String placesTravelled;
+    private String wishlist;
+    private String profilePicUrl;
+}
