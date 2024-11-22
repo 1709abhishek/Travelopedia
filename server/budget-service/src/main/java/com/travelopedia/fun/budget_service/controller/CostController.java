15d7155103d682e3f4d2ff35dc529d47ab356a13
Rahul Bagul <rahul07bagul@gmail.com>
2024-11-21 22:45:14 -0500

    Updated CostController

--- server/budget-service/src/main/java/com/travelopedia/fun/budget_service/controller/CostController.java
+++ server/budget-service/src/main/java/com/travelopedia/fun/budget_service/controller/CostController.java
@@ -3,6 +3,7 @@ package com.travelopedia.fun.budget_service.controller;
 
 import com.travelopedia.fun.budget_service.configuration.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.travelopedia.fun.budget_service.service.CostService;
 import com.travelopedia.fun.budget_service.beans.ItineraryRequest;
@@ -12,12 +13,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/budgets")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CostController {
+
+    @Autowired
+    private Configuration configuration;
 
     @Autowired
     private CostService costService;
 
-    @PostMapping("/getCostItinerary")
+    @PostMapping("/getHotelsCost")
     public List<ItineraryResponse> getCostItinerary(@RequestBody ItineraryRequest request) {
         return costService.getCostItinerary(request);
     }
